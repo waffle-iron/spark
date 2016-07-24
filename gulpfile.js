3dc60d02d1caade15212dea6e7e903cd9bdc0f54
extr0py <extr0py@extropygames.com>
2016-07-23 23:59:56 -0700

    Add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require("gulp");
+var gulpTypings = require("gulp-typings");
+var path = require("path");
+var exec = require("child_process").exec;
+
+gulp.task("typings:browser", (cb) => {
+    installTypings(path.join(__dirname, "src", "browser"), cb);
+});
+
+function installTypings(typingsConfigPath, cb) {
+    console.log("Platform: " + process.platform);
+    console.log("Typings path: " + typingsConfigPath);
+    var commandFile = "typings.cmd";
+    if(process.platform !== "win32") {
+        commandFile = "typings";
+    }
+    var typingsPath = path.join(__dirname, "node_modules", ".bin", commandFile);
+
+    console.log("Typings command: " + typingsPath);
+    var child = exec(typingsPath + " install", { cwd: typingsConfigPath });
+    child.stdout.pipe(process.stdout);
+    child.stderr.pipe(process.stderr);
+
+    child.on("exit", () => {
+        cb();
+    });
+}
